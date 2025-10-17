6f43f49a67c3de45130fc1d80f4bfcf141e0b24e
jianfjin <jianfjin@gmail.com>
2025-10-17 15:37:45 +0200

    update firebase config

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,13 +5,13 @@ import { getDatabase, ref, push, set, query, orderByChild, limitToLast, get } fr
 // IMPORTANT: Replace these with your actual Firebase project credentials
 // Get these from Firebase Console > Project Settings > General > Your apps > Firebase SDK snippet
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAB9357TSlzfHy0ZXsJO6v1kJa6DJwbQgA",
+    authDomain: "waste-wizards-nl.firebaseapp.com",
+    databaseURL: "https://waste-wizards-nl-default-rtdb.firebaseio.com",
+    projectId: "waste-wizards-nl",
+    storageBucket: "waste-wizards-nl.firebasestorage.app",
+    messagingSenderId: "600388402623",
+    appId: "G-LQ8RNBP49X"
 };
 
 // Initialize Firebase
